6c8f336d64adb086d60b9546fffe55f43c68b973
grimama <grimaldi.amanda@gmail.com>
2017-11-13 17:02:04 -0600

    most recent update

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,22 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
+router.get('/', function(req, res, next){
+  User.findAll()
+  .then(function(users){
+    res.render('users', {
+      users: users,
+    })
+  })
+})
+
+router.get('/users/:id', function(req, res, next){
+
+})
 
 module.exports = {
   router: router
